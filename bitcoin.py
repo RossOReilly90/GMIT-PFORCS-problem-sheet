b4569c514e4ee25e2369587df16b77b837f5a56a
Ross <g00398832@gmit.ie>
2021-02-10 16:03:37 +0000

    Added error correction and fully commented

--- bitcoin.py
+++ bitcoin.py
@@ -1,28 +1,54 @@
 # This program displays the current value of Bitcoin 
 # Authour - Ross O'Reilly
 # Date - 08/02/21
-#
+
+# REFERENCES
+# Andrew Beatty - Lecturer notes
+# https://jsonlint.com - Helped me to see how JSON files are designed
+# https://www.w3schools.com/python/python_json.asp - Reserched JSON a bit more 
+
+
+# Allows http requests to be sent in python
 import requests
 
+# Displays program heading
 print ("\n-------------")
 print ("Bitcoin Value")
 print ("-------------\n")
+ 
+# These lines of code were provided by lecturer Andrew Beatty
+# URL of a JSON file that updates bitcoin values hourly was set as 'url'
+# The request for this json was made and stored in a variable called returnedData
+# The json data was the converted into bitCoinDict
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 returnedData = requests.get(url)
 bitCoinDict = returnedData.json()
+# print("bitCoinDict") - i deleted this after copied the data to jsonlint.com and 
+# figured out how to access the specific labels
 
+
+# This is the code i used to display the currency rates 
+# before i decided to create 3 variables and round up the rates
 #print ('"USD rate": $',bitCoinDict['bpi']['USD']['rate_float'])
 #print ('"GBP rate": £',bitCoinDict['bpi']['GBP']['rate_float'])
 #print ('"EUR rate": €',bitCoinDict['bpi']['EUR']['rate_float'])
+
+# Stores the float rate into each of the associated variables
+# I done this because it allows me to round these floats to 2 decimal places
+# Whereas the 'rate' was a string and therefore couldn't be rounded 
 usRate = (bitCoinDict['bpi']['USD']['rate_float'])
 gbpRate = (bitCoinDict['bpi']['GBP']['rate_float'])
 eurRate = (bitCoinDict['bpi']['EUR']['rate_float'])
 
+# Displays message and value of bitcoin in 3 different currencies 
+# rounded to 2 decimal places
 print ("The rate of Bitcoin in US dollars is:   $", round(usRate, 2))
 print ("The rate of Bitcoin in GB pounds is:    £", round(gbpRate, 2))
 print ("The rate of Bitcoin in Euros is:        €", round(eurRate, 2))
 
+# Border for visual effect
 print ("\n-------------")
 
-# jsonlint.com was the biggest help for me as it displayed the json file
-# in a clear readable way
+# NOTES
+# jsonlint.com was the biggest help for me as it displayed the json file in a clean manner
+# which i could then use to extract the information i needed
--- bmi.py
+++ bmi.py
@@ -1,18 +1,52 @@
-# This is a body mass index calculator
+# Body mass index calculator
+# 
+# This program will take in the users weight and height
+# and calculate the individuals BMI
+# Error catching is included to prevent invalid inputs for weight and height
+#
 # Author - Ross O'Reilly
 # Date - 30/01/21
 
-# References
+# REFERENCES
 # https://www.w3resource.com/python-exercises/python-basic-exercise-66.php
+# https://www.youtube.com/watch?v=KdMAj8Et4xk - Examples of error catching using try and execute 
 
-print("\n\n====================================")   	        # Added for visual effect
-print ("Body Mass Index\n\nPlease enter your details.")         # Prints the title of the program
-print("====================================")                   # Added for visual effect
-weight = float(input("Enter your weight in Kilograms:\t "))     # Prompts the user to enter weight and then stores the float value in the 'weight' variable
-height = float(input("Enter your height in Meters: \t "))       # Prompts the user to enter height and then stores the float value in the 'height' variable
+
+
+print("\n\n====================================")                   # Program banner  
+print ("Body Mass Index\n\nPlease enter your details.")         
+print("====================================")                   
+
+StayInLoop = True                                                   # Boolean variable to help with looping
+while StayInLoop == True:                                           # Beginning of weight loop - while StayInLoop is true
+    try:                                                            # Error correction - if input type is correct (in this case a float) 
+        weight = float(input("Enter your weight in Kilograms: "))   # Takes in user weight input and checks that is it a float. If it's not, program will go straight to 'except'
+        if 0< weight <600:                                          # If the input is a float and is between 0 and 600 kgs
+            StayInLoop = False                                      # Boolean is now false and therefore the while loop will break
+        else:                                                       # If the float value is outside of the weight range i set (0< - 600)
+            print("Please enter a realistic weight. ")              # An error message will display asking for a realistic value 
+            StayInLoop = True                                       # The boolean will stay true so the while loop begins again
+    except:                                                         # Error correction - if input type is wrong (not a float)
+        print("Please enter a numeric weight. ")                    # Error message displayed asking for a numeric value to be entered
+        continue                                                    # Used to end the current iteration in a loop - in this case restarts the while loop
+
+StayInLoop = True                                                               # Reset the boolean variable as it will be used again in same methid for height input
+while StayInLoop == True:                                                       # Beggining of height loop - while StayInLoop is true 
+    try:                                                                        # Error correction - if input type is correct (in this case a float)
+        heightCm = float(input("Please enter your weight in centimeters:"))     # Takes in user height input and checks that it is a float. If it's not, program will go straight to 'except'  
+        if 30.48 <= heightCm <= 304.8:                                          # If the input is less than 30.48cm (1ft) or more than 304.8cm (10ft)
+            StayInLoop = False                                                  # Boolean is now false and therefore the while loop will break
+        else:                                                                   # If the float value is outside of the height range mentioned 2 ines up
+            print("Please enter a valid height.")                               # An error message will be displayed asking for a valid height
+            StayInLoop = True                                                   # The boolean will stay true so the while loop begins again
+    except:                                                                     # Error correction - if the input type is wrong (not a float)
+        print("Please enter a numeric height. ")                                # Error message displayed asking for a numeric value to be entered
+        continue                                                                # Used to end the current iteration in a loop - this while loop
+
+height = heightCm / 100                                         # Converts cm to m by dividing by 100
 bmi = weight / (height*height)                                  # BMI formula is calculated and stored in the 'bmi' variable
 
-print ("\nYour body mass index is:\t", round(bmi,2))            # Displays message and the prints bmi value rounded to 2 decimal places
+print ("\nYour body mass index is:\t ", round(bmi,2))           # Displays message and the prints bmi value rounded to 2 decimal places
 if 0 <= bmi <= 18.99:                                           # If statement with 5 conditions
     print("You are Underweight")                                # If bmi is less than 19 a message will display saying underweight
 elif 19.0 <= bmi <= 23.99:
@@ -24,10 +58,12 @@ elif 30.0 <= bmi <= 39.99:
 else:                                                           
     print ("You are Extremely Overweight")                      # If bmi is over 40 a message will display saying extermely obese
 print("====================================")
-print ("\nThank you!\n\n")
+print ("\nThank you!\n")
 
 
 # NOTES
-#
 # Found a site for a good example of how to display text and get an input in one short line (lines 11 and 12) referenced above
 # Used the c++ syntax && for the conditions (wouldn't work). Python has a great shorthand way of doing this by putting the variables between two numbers
+# If required, could use reg ex to prevent user from crashing program by inputting text instead of numbers
+# Tried this but didn't find it to be great in this situation
+# Looked up youtube and found a good video on how to get the type of input you want by doing error correction
